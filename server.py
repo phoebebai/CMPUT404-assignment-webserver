a7fa2a889961c38d7718c915d83cb0948f3074db
phoebebai <35245216+phoebebai@users.noreply.github.com>
2020-01-24 00:10:44 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -30,9 +31,55 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        alist = self.data.split()
+        
+        if len(alist) < 2:
+            return
+        if alist[0].decode("utf-8") != "GET":
+            cont = "<h1 align=\"center\">Error 405</h1>"
+            cont = cont.encode("utf-8")
+            length = len(cont)
+            cont = cont.decode("utf-8")
+
+            finalcont = "HTTP/1.x 405 Method Not Allowed\r\nContent-length:"+str(length)+"\r\nContent-Type: text/html\r\n\r\n<h1 align=\"center\">Error 405</h1>"
+            self.request.sendall(bytearray(finalcont,'utf-8'))
+            return
+
+        filesrc = "www" + alist[1].decode("utf-8")
+        if filesrc[-1] == "/":
+            filesrc = filesrc + "index.html"
+        else:
+            if os.path.exists(filesrc) and not os.path.isfile(filesrc):
+                finalcont = "HTTP/1.x 301 Moved Permanently\r\nContent-length: 0\r\nLocation: http://127.0.0.1:8080/deep/\r\nConnection: close\r\n\r\n"
+                self.request.sendall(bytearray(finalcont,'utf-8'))
+                return
+
+        try :
+            f = open(filesrc,"r")
+            cont = f.read()
+            cont = cont.encode("utf-8")
+            length = len(cont)
+            cont = cont.decode("utf-8")
+
+            if ".css" in filesrc:
+                finalcont = "HTTP/1.x 200 ok\r\nContent-length:"+str(length)+"\r\nContent-Type: text/css\r\n\r\n" + cont
+            elif".html" in filesrc:
+                finalcont = "HTTP/1.x 200 ok\r\nContent-length:"+str(length)+"\r\nContent-Type: text/html\r\n\r\n" + cont
+
+            self.request.sendall(bytearray(finalcont,'utf-8'))
+        
+        except :
+
+            cont = "<h1 align=\"center\">Error 404</h1>"
+            cont = cont.encode("utf-8")
+            length = len(cont)
+            cont = cont.decode("utf-8")
+            
+            finalcont = "HTTP/1.x 404 Not Found\r\nContent-length:"+str(length)+"\r\nContent-Type: text/html\r\n\r\n<h1 align=\"center\">Error 404</h1>"
+            self.request.sendall(bytearray(finalcont,'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -41,6 +88,6 @@ if __name__ == "__main__":
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
-    # Activate the server; this will keep running until you
+    # Activate the server; sthis will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
